82f0a62b14a1bed86dc291a7787c48c685966a51
jamuna Adhikari <adhikarijam@gmail.com>
2021-01-18 17:53:03 -0800

    Added Solutions

--- Question40.java
+++ Question40.java
@@ -1,4 +1,26 @@
 //Write a program to reverse the string.
-public class Question40 {
-
+import java.util.Scanner;
+public class Question40 
+{
+	public void reverseString(String word)
+	{
+		int length = word.length();
+		System.out.printf("Reverse String: ");
+		for(int i = length; i > 0; i--)
+		{
+			System.out.print(word.charAt(i-1));
+		}
+	}
+	public static void main(String[] args)
+	{
+		Scanner console = new Scanner(System.in);
+		
+		Question40 ques = new Question40();
+		
+		System.out.printf("Please enter a word: ");
+		String word = console.next();
+		
+		ques.reverseString(word);
+		
+	}
 }
